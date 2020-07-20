4f50052ba665696f326d660e29c32414a6dc3f5a
johnbxu <johnbxu@gmail.com>
2020-07-20 08:43:37 -0400

    Updates

---
+++ precache-manifest.360a9522ce42ef1b9c6d473a40724c91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbb501e1c24af0c9ff665f1415c776d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4398ef4b709ec6da99fc",
+    "url": "/monster-rolodex/static/css/main.6a8738d3.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4398ef4b709ec6da99fc",
+    "url": "/monster-rolodex/static/js/main.25619a66.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
